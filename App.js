018a89c69184e3dea2f0c142fc24ded60a05519d
Owen Ripley <ojripley@gmail.com>
2020-01-28 16:32:55 -0500

    add banana module

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-// export default class App extends Component {
+import Bananas from './Bananas.js';
 
-  // render() {
-  //   return (
-  //     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-  //       <Text>Hello, world!</Text>
-  //     </View>
-  //   );
-  // }
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+// export default function App() {
+export default class App extends Component {
+
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Hello, world!</Text>
+        <Bananas></Bananas>
+      </View>
+    );
+  }
+
+  // return (
+  //   <View style={styles.container}>
+  //     <Text>Open up App.js to start working on your app!</Text>
+  //   </View>
+  // );
 }
 
 const styles = StyleSheet.create({
---
+++ Bananas.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Image } from 'react-native';
+
+export default class Bananas extends Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+
+    return (
+      <Image source={pic} style={ {height: 110, width: 193} }></Image>
+    );
+  }
+}

40d2c98aacad254201c0121fe74443325464d2bb
Owen Ripley <ojripley@gmail.com>
2020-01-31 11:33:10 -0500

    comment out camera stuff. Making a seperate repo to play around with camera

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { RNCamera } from 'react-native-camera';
 
@@ -10,87 +10,87 @@ function getInitialState() {
   }
 }
 // export default function App() {
-export default class ExampleApp extends PureComponent {
+export default class App extends Component {
 
-
-  // render() {
-  //   return (
-  //     <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: '#000'}}>
-  //       <Text style={{color: 'white'}}>Hello, world!</Text>
-  //       <Bananas></Bananas>
-  //     </View>
-  //   );
-  // }
 
   render() {
     return (
-      <View style={styles.container}>
-        <RNCamera
-          ref={ref => {
-            this.camera = ref;
-          }}
-          style={styles.preview}
-          type={RNCamera.Constants.Type.back}
-          flashMode={RNCamera.Constants.FlashMode.on}
-          androidCameraPermissionOptions={{
-            title: 'Permission to use camera',
-            message: 'We need your permission to use your camera',
-            buttonPositive: 'Ok',
-            buttonNegative: 'Cancel',
-          }}
-          androidRecordAudioPermissionOptions={{
-            title: 'Permission to use audio recording',
-            message: 'We need your permission to use your audio',
-            buttonPositive: 'Ok',
-            buttonNegative: 'Cancel',
-          }}
-          onGoogleVisionBarcodesDetected={({ barcodes }) => {
-            console.log(barcodes);
-          }}
-        />
-        <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-          <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
-            <Text style={{ fontSize: 14 }}> SNAP </Text>
-          </TouchableOpacity>
-        </View>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: '#000'}}>
+        <Text style={{color: 'white'}}>Hello, world!</Text>
+        <Bananas></Bananas>
       </View>
-    )
+    );
   }
+
+  // render() {
+  //   return (
+  //     <View style={styles.container}>
+  //       <RNCamera
+  //         ref={ref => {
+  //           this.camera = ref;
+  //         }}
+  //         style={styles.preview}
+  //         type={RNCamera.Constants.Type.back}
+  //         flashMode={RNCamera.Constants.FlashMode.on}
+  //         androidCameraPermissionOptions={{
+  //           title: 'Permission to use camera',
+  //           message: 'We need your permission to use your camera',
+  //           buttonPositive: 'Ok',
+  //           buttonNegative: 'Cancel',
+  //         }}
+  //         androidRecordAudioPermissionOptions={{
+  //           title: 'Permission to use audio recording',
+  //           message: 'We need your permission to use your audio',
+  //           buttonPositive: 'Ok',
+  //           buttonNegative: 'Cancel',
+  //         }}
+  //         onGoogleVisionBarcodesDetected={({ barcodes }) => {
+  //           console.log(barcodes);
+  //         }}
+  //       />
+  //       <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+  //         <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
+  //           <Text style={{ fontSize: 14 }}> SNAP </Text>
+  //         </TouchableOpacity>
+  //       </View>
+  //     </View>
+  //   )
+  // }
   // return (
   //   <View style={styles.container}>
   //     <Text>Open up App.js to start working on your app!</Text>
   //   </View>
   // );
-  takePicture = async () => {
-    if (this.camera) {
-      const options = { quality: 0.5, base64: true };
-      const data = await this.camera.takePictureAsync(options);
-      console.log(data.uri);
-    }
-  };
+  // takePicture = async () => {
+  //   if (this.camera) {
+  //     const options = { quality: 0.5, base64: true };
+  //     const data = await this.camera.takePictureAsync(options);
+  //     console.log(data.uri);
+  //   }
+  // };
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'black',
-  },
-  preview: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  capture: {
-    flex: 0,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    padding: 15,
-    paddingHorizontal: 20,
-    alignSelf: 'center',
-    margin: 20,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     flexDirection: 'column',
+//     backgroundColor: 'black',
+//   },
+//   preview: {
+//     flex: 1,
+//     justifyContent: 'flex-end',
+//     alignItems: 'center',
+//   },
+//   capture: {
+//     flex: 0,
+//     backgroundColor: '#fff',
+//     borderRadius: 5,
+//     padding: 15,
+//     paddingHorizontal: 20,
+//     alignSelf: 'center',
+//     margin: 20,
+//   },
+// });
 
-AppRegistry.registerComponent('App', () => ExampleApp);
+// AppRegistry.registerComponent('App', () => ExampleApp);
 
